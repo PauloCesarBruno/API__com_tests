627b143a9c7faee325c7242984b919bec4263c15
Paulo Cesar C Bruno <p_bruno001@hotmail.com>
2023-11-19 06:19:35 -0300

    Add. Testes unitários   => xUNIT em Categorias

--- APICatalogoxUnitTests/CategoriasUnitTestsController.cs
+++ APICatalogoxUnitTests/CategoriasUnitTestsController.cs
@@ -5,6 +5,8 @@ using APICatalogo.Pagination;
 using APICatalogo.Repository;
 using APICatalogo.TestMokControllers;
 using AutoMapper;
+using FluentAssertions;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 namespace APICatalogoxUnitTests;
@@ -54,13 +56,18 @@ public class CategoriasUnitTestsController
         repository = new UnitOfWork(context);        
     }
 
-    // Inicio dos testes Unitários=============================================
+    // Inicio dos testes Unitário:
     // Casos de teste:
-    //=========================================================================
+    //=====================================================================================================
+
+    // ===================================================================================================
+    // TESTES DE CONSULTAS
+    // ===================================================================================================
 
     /* Testar o método GET do Controlador de CategoriasController:
-       Testar se o valor retornado é igual a umaLISTA de objeto Categria.*/
-    [Fact]    
+       Testar se o valor retornado é igual a uma LISTA de objeto Categria.*/
+
+    [Fact]  // Informa que é um código de teste unitário.  
     public async void GetCategorias_Return_OkResult()
     {
         //Arrange  
@@ -70,10 +77,162 @@ public class CategoriasUnitTestsController
             PageNumber = 1,
             PageSize = 10
         };
+
         //Act
         var data = await controller.Get(param);
+
         //Assert  
+        // Muita atenção para o "Assert", ele tem que seguir a lógica do teste desejado.
         Assert.IsType<CategoriaDTO>(data.Value.First());
     }
 
+    //=====================================================================================================
+
+    /* Testar o método GET (FORÇAR UM BADREQUEST) do Controlador de CategoriasController:
+   Testar se o valor retornado é igual a umaLISTA de objeto Categria.*/
+
+    [Fact] // Informa que é um código de teste unitário.  
+    public async void GetCategorias_Return_BadRequestResult()
+    {
+        //Arrange  
+        var controller = new CategoriasMockController(repository, mapper);
+        CategoriasParameters parameter = new CategoriasParameters()
+        {
+            PageNumber = 1,
+            PageSize = 10
+        };
+
+        //Act
+        var data = await controller.GetBad(parameter);                 
+
+        //Assert  
+        // Muita atenção para o "Assert", ele tem que seguir a lógica do teste desejado.
+        Assert.IsType<BadRequestResult>(data.Result);
+    }
+
+
+    //=====================================================================================================
+
+    /* Testar o método GET  do Controlador de CategoriasController:
+    retornar alguns dados que tenho no Banco de ados.*/
+
+    [Fact] // Informa que é um código de teste unitário.  
+    public async void GetCategorias_Return_MatchResult()
+    {
+        //Arrange  
+        var controller = new CategoriasMockController(repository, mapper);
+        CategoriasParameters parameter = new CategoriasParameters()
+        {
+            PageNumber = 1,
+            PageSize = 10
+        };
+
+        //Act
+        var data = await controller.GetBd(parameter);
+
+        //Assert  
+        // Muita atenção para o "Assert", ele tem que seguir a lógica do teste desejado.
+        Assert.IsType<List<CategoriaDTO>>(data.Value);
+        // Avbaixo atribuo uma lista de categorias no objeto categorias e vou ter uma lista de objetos.
+        var cat = data.Value.Should().BeAssignableTo<List<CategoriaDTO>>().Subject;
+
+        Assert.Equal("Bebidas", cat[0].Nome); // Índice [0] => primeiro registro do meu Db.
+        Assert.Equal("bebidas.jpg", cat[0].ImagemUrl); // Índice [0] => primeiro registro do meu Db.
+
+        Assert.Equal("Sobremesas", cat[2].Nome); // Índice [2] => terceiro registro do meu Db.
+        Assert.Equal("sobremesas.jpg", cat[2].ImagemUrl); // Índice [2] => terceiro registro do meu Db.
+    }
+
+
+    //=====================================================================================================
+
+    /* Testar o método GET  do Controlador de CategoriasController:
+    retornar Categoria por um Id - retornatr um ObjetoDTO por Id.*/
+
+    [Fact] // Informa que é um código de teste unitário.  
+    public async void GetCategoriaById_Return_OkResult()
+    {
+        //Arrange  
+        var controller = new CategoriasMockController(repository, mapper);
+        var catId = 2;
+
+        //Act
+        var data = await controller.GetById(catId);
+
+        //Assert  
+        // Muita atenção para o "Assert", ele tem que seguir a lógica do teste desejado.
+        Assert.IsType<CategoriaDTO>(data.Value);
+    }
+
+    //=====================================================================================================
+
+    /* Testar o método GET  do Controlador de CategoriasController:
+    retornar Categoria por um Id - retornatr um ObjetoDTO por Id.*/
+
+    [Fact] // Informa que é um código de teste unitário.  
+    public async void GetCategoriaById_Return_NotFoundResult()
+    {
+        //Arrange  
+        var controller = new CategoriasMockController(repository, mapper);
+        var catId = 9999;
+
+        //Act
+        var data = await controller.GetById(catId);
+
+        //Assert  
+        // Muita atenção para o "Assert", ele tem que seguir a lógica do teste desejado.
+        Assert.IsType<NotFoundResult>(data.Result);
+    }
+
+    /// ===================================================================================================
+    // TESTES DE MANIPULAÇÃ - POST / PUT / DELETE   
+    // ===================================================================================================
+
+    /* Testar o método post  do Controlador de CategoriasController:
+     tem que retornar um CreatedAtRouteResult.*/
+
+    [Fact] // Informa que é um código de teste unitário.  
+    public async void Post_Categoria_AddValidData_Return_CreatedResult()
+    {
+        //Arrange  
+        var controller = new CategoriasMockController(repository, mapper);
+
+        // Vai ser incluido esse objeto categoria - Vai ser incluido na base de dados.
+        var categ = new CategoriaDTO()
+        { Nome = "Teste_inclusao", ImagemUrl = "testcatInclusao.jpg" };
+
+        //Act
+        var data = await controller.Post(categ);
+
+        //Assert  
+        // Muita atenção para o "Assert", ele tem que seguir a lógica do teste desejado.
+        // O Retorno terá que ser um CreatedAtRouteResult.
+        Assert.IsType<CreatedAtRouteResult>(data);
+    }
+
+    /* Este método Put tera que Alterar uma categoria,
+     retornar um StatusCode 200, além de  ALTERAR epersistir o dado no Banco de dados*/
+    [Fact] // Informa que é um código de teste unitário.  
+    public async void Put_Categoria_Update_ValidData_Return_OkResult()
+    {
+        //Arrange  
+        var controller = new CategoriasMockController(repository, mapper);
+        var catId = 12;
+       
+        //Act
+        var existingPost = await controller.GetById(catId);
+        var result = existingPost.Value.Should().BeAssignableTo<CategoriaDTO>().Subject;
+
+        var catDto = new CategoriaDTO();
+        catDto.CategoriaId = catId;
+        catDto.Nome = "Categoria Atualizada - Testes 1000";
+        catDto.ImagemUrl = result.ImagemUrl;        
+        
+        var updateData = controller.Put(catId, catDto);
+
+        //Assert  
+        // Muita atenção para o "Assert", ele tem que seguir a lógica do teste desejado.
+        // O Retorno terá que ser um 200-Ok e alterar o registro especivicado no (var catId).
+        Assert.IsType<OkResult>(updateData);
+    }
 }
--- APICatalogoxUnitTests/TestMokControllers/CategoriasMockController.cs
+++ APICatalogoxUnitTests/TestMokControllers/CategoriasMockController.cs
@@ -1,33 +1,39 @@
-﻿using APICatalogo.Controllers;
-using APICatalogo.DTOs;
+﻿using APICatalogo.DTOs;
+using APICatalogo.Models;
 using APICatalogo.Pagination;
 using APICatalogo.Repository;
 using AutoMapper;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
+using System.Data;
 
-namespace APICatalogo.TestMokControllers
+namespace APICatalogo.TestMokControllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class CategoriasMockController : ControllerBase
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class CategoriasMockController : ControllerBase
-    {
-        private readonly IUnitOfWork _context;            
-        private readonly IMapper _mapper;
+    private readonly IUnitOfWork _context;
+    private readonly IMapper _mapper;
 
-        public CategoriasMockController(IUnitOfWork context, IMapper mapper)
-        {
-            _context = context;               
-            _mapper = mapper;
-        }
-            
-        [HttpGet]
-        public async Task<ActionResult<IEnumerable<CategoriaDTO>>>
-                Get([FromQuery] CategoriasParameters categoriasParameters)
+    public CategoriasMockController(IUnitOfWork context, IMapper mapper)
+    {
+        _context = context;
+        _mapper = mapper;
+    }
+
+    // ===================================================================================================
+    // TESTES DE CONSULTAS
+    // ===================================================================================================
+     
+    [HttpGet] // Para teste de retorno de ima coleção de objetos
+    public async Task<ActionResult<IEnumerable<CategoriaDTO>>>
+            Get([FromQuery] CategoriasParameters categoriasParameters)
+    {
+        try
         {
             var categorias = await _context.CategoriaRepository.
-                                GetCategorias(categoriasParameters);
+                           GetCategorias(categoriasParameters);
             var metadata = new
             {
                 categorias.TotalCount,
@@ -43,5 +49,130 @@ namespace APICatalogo.TestMokControllers
             var categoriasDto = _mapper.Map<List<CategoriaDTO>>(categorias);
             return categoriasDto;
         }
+        catch (Exception)
+        {
+
+            return BadRequest();
+        }
     }
+
+    [HttpGet("badrequest")] // Para teste (FORÇAR UM BADREQUEST) como retorto
+    public async Task<ActionResult<IEnumerable<CategoriaDTO>>>
+               GetBad([FromQuery] CategoriasParameters categoriasParameters)
+    {
+        try
+        {
+            var categorias = await _context.CategoriaRepository.
+                           GetCategorias(categoriasParameters);
+            var metadata = new
+            {
+                categorias.TotalCount,
+                categorias.PageSize,
+                categorias.CurrentPage,
+                categorias.TotalPages,
+                categorias.HasNext,
+                categorias.HasPrevious
+            };
+
+            //Response.Headers.Add("X-Pagination", JsonConvert.SerializeObject(metadata));
+
+            var categoriasDto = _mapper.Map<List<CategoriaDTO>>(categorias);
+            throw new Exception();
+            // return categoriasDto;
+        }
+        catch (Exception)
+        {
+
+            return BadRequest();
+        }
+    }
+
+    [HttpGet("bd")] // Teste para retornar o 1º e o 3º Registro do meu Banco de Dados.
+    public async Task<ActionResult<IEnumerable<CategoriaDTO>>>
+              GetBd([FromQuery] CategoriasParameters categoriasParameters)
+    {
+        try
+        {
+            var categorias = await _context.CategoriaRepository.
+                           GetCategorias(categoriasParameters);
+            var metadata = new
+            {
+                categorias.TotalCount,
+                categorias.PageSize,
+                categorias.CurrentPage,
+                categorias.TotalPages,
+                categorias.HasNext,
+                categorias.HasPrevious
+            };
+
+            //Response.Headers.Add("X-Pagination", JsonConvert.SerializeObject(metadata));
+
+            var categoriasDto = _mapper.Map<List<CategoriaDTO>>(categorias);
+            return categoriasDto;
+        }
+        catch (Exception)
+        {
+
+            return BadRequest();
+        }
+    }
+
+    [HttpGet("{id}", Name = "ObterCategoria")] /*Este método "Mock" serve paara testar 2 (duas)
+    situações, uma é se vai retornar um result de um Objeto pelo se Id e a outra situação é ver
+    se vai retornar um "NotFopud" se for passado um Id Inexistente.*/
+    public async Task<ActionResult<CategoriaDTO>> GetById(int id)
+    {
+        var categoria = await _context.CategoriaRepository
+                                .GetById(c => c.CategoriaId == id);
+        if (categoria == null)
+        {
+
+            return NotFound();
+        }
+
+        var categoriaDTO = _mapper.Map<CategoriaDTO>(categoria);
+
+        return categoriaDTO;
+    }
+
+    /// ===================================================================================================
+    // TESTES DE MANIPULAÇÃ - POST / PUT / DELETE   
+    // ===================================================================================================
+
+    [HttpPost] /* Este método Post tera que retornar no teste um CreatedAtRouteResult,
+     além de persistir o dado no Banco de dados*/
+    public async Task<ActionResult> Post([FromBody] CategoriaDTO categoriaDto)
+    {
+        var categoria = _mapper.Map<Categoria>(categoriaDto);
+
+        _context.CategoriaRepository.Add(categoria);
+        await _context.Commit();
+
+        var categoriaDTO = _mapper.Map<CategoriaDTO>(categoria);
+
+        return new CreatedAtRouteResult("ObterCategoria",
+            new { id = categoria.CategoriaId }, categoriaDTO);
+    }
+
+
+
+    [HttpPut("{id}")]  /* Este método Put tera que Alterar uma categoria,
+     retornar um StatusCode 200, além de persistir o dado no Banco de dados*/
+    public ActionResult Put(int id, [FromBody] CategoriaDTO categoriaDto)
+    {
+        if (id != categoriaDto.CategoriaId)
+        {
+            return BadRequest();
+        }
+
+        var categoria = _mapper.Map<Categoria>(categoriaDto);
+
+        _context.CategoriaRepository.Update(categoria);
+        _context.Commit();
+
+        return Ok();
+    }
+   
 }
+
+
